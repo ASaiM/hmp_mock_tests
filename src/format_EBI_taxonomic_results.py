2d3d7c87b0171cd3c54118341c02c481a4d3b73e
bebatut <berenice.batut@gmail.com>
2016-03-08 10:17:24 +0100

    Modify formatting of EBI taxonomic results
    - compute relative abundance of clade only for OTUs with a precise
    taxonomic assignation
    - extract unassigned clades at each levels

--- src/format_EBI_taxonomic_results.py
+++ src/format_EBI_taxonomic_results.py
@@ -6,47 +6,35 @@ import os
 import argparse
 import re
 
-def incremente_taxonomy(taxo_levels, taxo_level_abundances):
-    if len(taxo_levels) == 0:
-        taxo_level_abundances.setdefault('Unassigned',0)
-        taxo_level_abundances['Unassigned'] += 1
-        return taxo_level_abundances
+taxo_level_order = ['domain','kingdom','phylum','class','order','family']
 
-    taxo = taxo_levels[0]
-    taxo = taxo.split('__')[1]
-    taxo = taxo.replace("_"," ")
+def incremente_taxonomy(taxo_levels, taxo_level_abundances, taxo_level_order):
+    taxo_level_abundances.setdefault('abundances',
+        { 'all': 0, 'unassigned': 0, 'assigned': 0})
+    taxo_level_abundances['abundances']['all'] += 1
+    taxo_level_abundances.setdefault('subclades', {})
+    if len(taxo_levels) == 0 :
+        if len(taxo_level_order) == 0:
+            taxo_level_abundances['abundances']['assigned'] += 1
+        else:
+            taxo_level_abundances['abundances']['unassigned'] += 1
+    else:
+        
+        taxo = taxo_levels[0]
+        taxo = taxo.split('__')[1]
+        taxo = taxo.replace("_"," ")
 
-    if taxo != '':
-        taxo_level_abundances.setdefault(taxo,{})
-        taxo_level_abundances[taxo].setdefault('All', 0)
-        taxo_level_abundances[taxo]['All'] += 1
-        taxo_level_abundances[taxo] = incremente_taxonomy(taxo_levels[1:],
-            taxo_level_abundances[taxo])
+        if taxo != '':
+            taxo_level_abundances['subclades'].setdefault(taxo,{})
+            taxo_level_abundances['subclades'][taxo] = incremente_taxonomy(
+                taxo_levels[1:], taxo_level_abundances['subclades'][taxo],
+                taxo_level_order[1:])
     return taxo_level_abundances
 
-def write_taxo_levels(taxo_level_abundances, all_taxo_level_abundance_file, 
-        taxo_levels_abundance_files, taxo_level_order, previous_levels, otu_nb):
-    if taxo_level_abundances.has_key('All'):
-        abundance = 100*taxo_level_abundances['All']/(1.*otu_nb)
-        
-        all_taxo_level_abundance_file.write('\t'.join(previous_levels))
-        all_taxo_level_abundance_file.write('\t'*len(taxo_level_order[1:]))
-        all_taxo_level_abundance_file.write('\t' + str(abundance) + '\n')
-
-        taxo_levels_abundance_files[taxo_level_order[0]].write(previous_levels[-1] + '\t')
-        taxo_levels_abundance_files[taxo_level_order[0]].write(str(abundance) + '\n')
-
-    for taxo in taxo_level_abundances:
-        if taxo != 'All' and taxo != 'Unassigned':
-            write_taxo_levels(taxo_level_abundances[taxo], 
-                all_taxo_level_abundance_file,
-                taxo_levels_abundance_files, taxo_level_order[1:], 
-                previous_levels + [taxo], otu_nb)
-
-def format_EBI_taxonomic_results(args):
+def extract_taxo_level_abundances(input_filepath):
     taxo_level_abundances = {}
 
-    with open(args.ebi_taxonomic_results, 'r') as input_file:
+    with open(input_filepath, 'r') as input_file:
         otu_nb = 0
         for line in input_file.readlines():
             split_line = line[:-1].split('\t')
@@ -54,39 +42,74 @@ def format_EBI_taxonomic_results(args):
             all_taxo = line[:-1].split('\t')[1]
             taxo_levels = all_taxo.split(';')[1:]
             taxo_level_abundances = incremente_taxonomy(taxo_levels, 
-                taxo_level_abundances)
+                taxo_level_abundances, taxo_level_order[1:])
 
+    return taxo_level_abundances, otu_nb
+
+def get_precisely_assigned_otu_abundances(taxo_level_abundances):
+    for taxo in taxo_level_abundances['subclades']:
+        taxo_level_abundances['subclades'][taxo], assigned_otu_nb = get_precisely_assigned_otu_abundances(
+            taxo_level_abundances['subclades'][taxo])
+        taxo_level_abundances['abundances']['assigned'] += assigned_otu_nb
+    return taxo_level_abundances, taxo_level_abundances['abundances']['assigned']
+
+def write_taxo_levels(taxo_level_abundances, all_taxo_level_abundance_file, 
+        taxo_levels_abundance_files, taxo_level_order, previous_levels, 
+        info_type, normalization_value = None):
+    abundance = 100*taxo_level_abundances['abundances'][info_type]
+    if info_type != 'unassigned':
+        abundance /= (1.*normalization_value)
+    else:
+        abundance /= (1.*taxo_level_abundances['abundances']['all'])
+    
+    all_taxo_level_abundance_file.write('\t'.join(previous_levels))
+    all_taxo_level_abundance_file.write('\t'*len(taxo_level_order[1:]))
+    all_taxo_level_abundance_file.write('\t' + str(abundance) + '\n')
+
+    taxo_levels_abundance_files[taxo_level_order[0]].write(previous_levels[-1] + '\t')
+    taxo_levels_abundance_files[taxo_level_order[0]].write(str(abundance) + '\n')
+
+    if taxo_level_abundances.has_key('subclades'):
+        for taxo in taxo_level_abundances['subclades']:
+            write_taxo_levels(taxo_level_abundances['subclades'][taxo], 
+                all_taxo_level_abundance_file, taxo_levels_abundance_files, 
+                taxo_level_order[1:], previous_levels + [taxo], info_type,
+                normalization_value)
+
+def write_abundances(taxo_level_abundances, output_dir, info_type, otu_nb = None):
     taxo_levels_abundance_files = {}
-    taxo_levels_abundance_files['kingdom'] = open(args.kingdom_abundance_file, 'w')
-    taxo_levels_abundance_files['phylum'] = open(args.phylum_abundance_file, 'w')
-    taxo_levels_abundance_files['class'] = open(args.class_abundance_file, 'w')
-    taxo_levels_abundance_files['order'] = open(args.order_abundance_file, 'w')
-    taxo_levels_abundance_files['family'] = open(args.family_abundance_file, 'w')
-    all_taxo_level_abundance_file = open(args.all_taxo_level_abundance_file, 'w')
-
-    taxo_level_order = ['domain','kingdom','phylum','class','order','family']
-    for taxo_level in taxo_level_order[1:]:
+    all_taxo_level_abundance_file = open(output_dir + 
+        '/all_taxo_level_' + info_type + '_abundance_file.txt', 'w')
+    for taxo_level in taxo_level_order:
+        taxo_levels_abundance_files[taxo_level] = open(output_dir + 
+            '/' + taxo_level + '_' + info_type + '_abundance.txt', 'w')
         taxo_levels_abundance_files[taxo_level].write(taxo_level + '\t')
         taxo_levels_abundance_files[taxo_level].write('abundance\n')
         all_taxo_level_abundance_file.write(taxo_level + '\t')
     all_taxo_level_abundance_file.write('abundance\n')    
 
     write_taxo_levels(taxo_level_abundances, all_taxo_level_abundance_file, 
-        taxo_levels_abundance_files, taxo_level_order, [], otu_nb)
+        taxo_levels_abundance_files, taxo_level_order, [''], info_type, otu_nb)
 
     for taxo_level_file in taxo_levels_abundance_files:
         taxo_levels_abundance_files[taxo_level_file].close()
     all_taxo_level_abundance_file.close()
 
+def format_EBI_taxonomic_results(args):
+    taxo_level_abundances, otu_nb = extract_taxo_level_abundances(
+        args.ebi_taxonomic_results)
+    taxo_level_abundances, assigned_otu_nb = get_precisely_assigned_otu_abundances(
+        taxo_level_abundances)
+
+    write_abundances(taxo_level_abundances, args.output_dir, 'all', otu_nb)
+    write_abundances(taxo_level_abundances, args.output_dir, 'unassigned')
+    write_abundances(taxo_level_abundances, args.output_dir, 'assigned', 
+        assigned_otu_nb)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--ebi_taxonomic_results', required=True)
-    parser.add_argument('--all_taxo_level_abundance_file', required=True)
-    parser.add_argument('--kingdom_abundance_file', required=True)
-    parser.add_argument('--phylum_abundance_file', required=True)
-    parser.add_argument('--class_abundance_file', required=True)
-    parser.add_argument('--order_abundance_file', required=True)
-    parser.add_argument('--family_abundance_file', required=True)
+    parser.add_argument('--output_dir', required=True)
     args = parser.parse_args()
 
     format_EBI_taxonomic_results(args)
