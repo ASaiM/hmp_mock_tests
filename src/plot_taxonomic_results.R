31ef812d60c5fe06fbcdd1a6c355a44b50c2e3ba
bebatut <berenice.batut@gmail.com>
2016-03-08 17:30:41 +0100

    Add script to plot taxonomic results

---
+++ src/plot_taxonomic_results.R
@@ -0,0 +1,22 @@
+args<-commandArgs(TRUE)
+
+output_dir = args[1]
+
+family_abundances = read.table(paste(output_dir, "/family_abundance.txt", sep = ''), h = T, sep = '\t', row.names = 1)
+family_abundances[family_abundances == 0] = NA
+#differences = cbind(family_abundances$ebi_abundance - family_abundances$expected,
+#    family_abundances$asaim_abundance - family_abundances$expected)
+#rownames(differences) = rownames(family_abundances)
+#colnames(differences) = c("ebi", "asaim")
+
+pdf(paste(output_dir, "/family_abundances.pdf", sep = ""))
+par(las=2)
+par(mar=c(5,9,1,1))
+col = c("blue3","green3", "red")
+order = c(12,15,1,5,6,7,10,14,4,8,9,11,16,18,2,3,13,17,19)
+barplot(t(rev(family_abundances[order,])), 
+    width = c(1,1,0.75), col= col, xlab = "Relative abundances in percentage", 
+    beside=TRUE, horiz=TRUE, log = "x", cex.names=0.7, cex.axis = 0.8)
+legend("topright", legend = rev(c('ASaiM','EBI','Expected')), fill = rev(col), cex = 0.75)
+dev.off()
+
