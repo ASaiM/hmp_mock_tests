dcd959dfceb5cdb664b82d2699253e3e8f5f974a
bebatut <berenice.batut@gmail.com>
2016-03-09 14:17:16 +0100

    Add template for bioinformatics

--- doc/build.sh
+++ doc/build.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-pandoc -s doc/report.md \
+cd doc
+pandoc -s report.md \
     -t latex \
-    -o doc/report.pdf \
-    --filter pandoc-fignos
+    -o report.pdf \
+    --template="bioinformatics_template.tex" \
+    -N
+
+#pandoc -s doc/report.md \
+#    -t docx \
+#    -o doc/report.docx \
+#    --filter pandoc-fignos \
+#    --reference-docx="doc/cabios_word_temp/MS Word Template Bioinformatics.dotx" \
+#    --template="doc/cabios_word_temp/MS Word Template Bioinformatics.dotx"
