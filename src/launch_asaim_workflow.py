2e4855dc816677f1fa45e7a100c2c92075228615
bebatut <berenice.batut@gmail.com>
2016-03-16 16:57:55 +0100

    Rewrite to add galaxy_api_commands

--- src/launch_asaim_workflow.py
+++ src/launch_asaim_workflow.py
@@ -7,44 +7,16 @@ import argparse
 import re
 import time
 from bioblend import galaxy
+import galaxy_api_commands
 
 def launch_asaim_workflow(args):
     workflow_file_path = "data/asaim_galaxy_workflow.ga"
-    input_data_file = "data/" + args.sample_name + ".fastq"
+    gi = galaxy_api_commands.connect_to_galaxy_instance(args.gi_url, args.api_key)
 
-    print "  Connect to Galaxy instance on ", args.gi_url
-    gi = galaxy.GalaxyInstance(url=args.gi_url, key=args.api_key)
+    input_filepaths = {'raw sequences': "data/" + args.sample_name + ".fastq"}
 
-    print "  Create an history for ", args.sample_name, " and import input data"
-    history_details = gi.histories.create_history(args.sample_name)
-    hist_id = history_details['id']
-    upload_file_details = gi.tools.upload_file(input_data_file, hist_id)
-    upload_file_job_id = upload_file_details['jobs'][0]['id']
-    while str(gi.jobs.get_state(upload_file_job_id)) != 'ok':
-        time.sleep(1)
-
-    print "  Import workflow and launch it"
-    wf_details = gi.workflows.import_workflow_from_local_path(workflow_file_path)
-    wf_id = wf_details['id']
-    wf = gi.workflows.show_workflow(wf_id)
-    wf_input_id = wf['inputs'].keys()[0]
-    dataset_id = gi.histories.show_history(hist_id, contents=False)['state_ids']['ok'][0]
-    datamap = dict()
-    datamap[wf_input_id] = { 'src':'hda', 'id':dataset_id }
-    wf_invocation_details = gi.workflows.run_workflow(wf_id, datamap, 
-        history_id=hist_id)
-    wf_invocation_id = wf_invocation_details['id']
-    wf_outputs = wf_invocation_details['outputs']
-    
-    print "  Wait while workflow is running and export output datasets when generated"
-    output_dir_path = "results/" + args.sample_name + "/asaim_results/" 
-    if not os.path.exists(output_dir_path):
-        os.mkdir(output_dir_path)
-    for dataset_id in wf_outputs:
-        while str(gi.datasets.show_dataset(dataset_id)['state']) != 'ok':
-            time.sleep(1)
-            print dataset_id, gi.datasets.show_dataset(dataset_id)['state']
-        gi.histories.download_dataset(hist_id, dataset_id, output_dir_path)
+    galaxy_api_commands.run_workflow(args.sample_name, workflow_file_path, input_filepaths, 
+        gi, '', export = False)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
