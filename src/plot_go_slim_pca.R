e39b55b1c73636490556a884a8bf2bb90c6bed43
bebatut <berenice.batut@gmail.com>
2016-03-21 17:35:53 +0100

    Add some legend in pca plots

--- src/plot_go_slim_pca.R
+++ src/plot_go_slim_pca.R
@@ -11,9 +11,13 @@ get_data <- function(type){
     colnames(l$extracted_data) = c('SRR072232 (ASaiM)', 'SRR072232 (EBI)', 'SRR072233 (ASaiM)', 'SRR072233 (EBI)')
 
     l$pca <- dudi.pca(l$extracted_data, scannf = FALSE, nf = 2)
-    l$order = order(abs(l$pca$li[,1]),decreasing = T)
-    l$order_axis1 = l$pca$li[l$order,]
-    l$go_names = l$data[l$order,2]
+    l$order_1 = order(abs(l$pca$li[,1]),decreasing = T)
+    l$order_axis1 = l$pca$li[l$order_1,]
+    l$go_names_axis1 = l$data[l$order_1,2]
+
+    l$order_2 = order(abs(l$pca$li[,2]),decreasing = T)
+    l$order_axis2 = l$pca$li[l$order_2,]
+    l$go_names_axis2 = l$data[l$order_2,2]
     return(l)
 }
 
@@ -23,11 +27,14 @@ pdf(paste('results/concatenated_samples/', type, '/pca.pdf', sep = ''))
 scatter(bp$pca, clab.row = 0, posieig = "none", sub = paste('First axis (' ,
     round(100*bp$pca$eig[1]/sum(bp$pca$eig)), '%) - Second axis (', 
     round(100*bp$pca$eig[2]/sum(bp$pca$eig)),'%)', sep = ''))
-text(bp$order_axis1[1,1],bp$order_axis1[1,2], label = bp$go_names[1], pos = 4)
-text(bp$order_axis1[2,1],bp$order_axis1[2,2], label = bp$go_names[2], pos = 3)
-text(bp$order_axis1[3,1],bp$order_axis1[3,2], label = bp$go_names[3], pos = 3)
-text(bp$order_axis1[4,1],bp$order_axis1[4,2], label = bp$go_names[4], pos = 2)
-text(bp$order_axis1[5,1],bp$order_axis1[5,2], label = bp$go_names[5], pos = 3)
+text(bp$order_axis1[1,1],bp$order_axis1[1,2], label = bp$go_names_axis1[1], pos = 4)
+text(bp$order_axis1[2,1],bp$order_axis1[2,2], label = bp$go_names_axis1[2], pos = 3)
+text(bp$order_axis1[3,1],bp$order_axis1[3,2], label = bp$go_names_axis1[3], pos = 3)
+text(bp$order_axis1[4,1],bp$order_axis1[4,2], label = bp$go_names_axis1[4], pos = 2)
+
+text(bp$order_axis2[1,1],bp$order_axis2[1,2], label = bp$go_names_axis2[1], pos = 3)
+text(bp$order_axis2[2,1],bp$order_axis2[2,2], label = bp$go_names_axis2[2], pos = 3)
+text(bp$order_axis2[4,1],bp$order_axis2[4,2], label = bp$go_names_axis2[4], pos = 3)
 dev.off()
 
 type = 'cellular_component'
@@ -36,9 +43,9 @@ pdf(paste('results/concatenated_samples/', type, '/pca.pdf', sep = ''))
 scatter(cc$pca, clab.row = 0, posieig = "none", sub = paste('First axis (' ,
     round(100*cc$pca$eig[1]/sum(cc$pca$eig)), '%) - Second axis (', 
     round(100*cc$pca$eig[2]/sum(cc$pca$eig)),'%)', sep = ''))
-text(cc$order_axis1[1,1],cc$order_axis1[1,2], label = cc$go_names[1], pos = 4)
-text(cc$order_axis1[2,1],4.1, label = cc$go_names[2], pos = 1)
-text(cc$order_axis1[3,1],-2.3, label = cc$go_names[3], pos = 3)
+text(cc$order_axis1[1,1],cc$order_axis1[1,2], label = cc$go_names_axis1[1], pos = 4)
+text(cc$order_axis1[2,1],4.1, label = cc$go_names_axis1[2], pos = 1)
+text(cc$order_axis1[3,1],-2.3, label = cc$go_names_axis1[3], pos = 3)
 #text(cc$order_axis1[4,1],cc$order_axis1[4,2], label = cc$go_names[4], pos = 3)
 #text(cc$order_axis1[5,1],cc$order_axis1[5,2], label = cc$go_names[5], pos = 3)
 dev.off()
@@ -49,9 +56,11 @@ pdf(paste('results/concatenated_samples/', type, '/pca.pdf', sep = ''))
 scatter(mf$pca, clab.row = 0, posieig = "none", sub = paste('First axis (' ,
     round(100*mf$pca$eig[1]/sum(mf$pca$eig)), '%) - Second axis (', 
     round(100*mf$pca$eig[2]/sum(mf$pca$eig)),'%)', sep = ''))
-text(mf$order_axis1[1,1],mf$order_axis1[1,2], label = mf$go_names[1], pos = 4)
-text(mf$order_axis1[2,1],mf$order_axis1[2,2], label = mf$go_names[2], pos = 1)
-text(mf$order_axis1[3,1],mf$order_axis1[3,2], label = mf$go_names[3], pos = 3)
-text(mf$order_axis1[4,1],mf$order_axis1[4,2], label = mf$go_names[4], pos = 2)
-text(mf$order_axis1[5,1],mf$order_axis1[5,2], label = mf$go_names[5], pos = 3)
+text(mf$order_axis1[1,1],mf$order_axis1[1,2], label = mf$go_names_axis1[1], pos = 4)
+text(mf$order_axis1[2,1],mf$order_axis1[2,2], label = mf$go_names_axis1[2], pos = 1)
+text(mf$order_axis1[3,1],mf$order_axis1[3,2], label = mf$go_names_axis1[3], pos = 3)
+text(mf$order_axis1[4,1],mf$order_axis1[4,2], label = mf$go_names_axis1[4], pos = 2)
+text(mf$order_axis1[5,1],mf$order_axis1[5,2], label = mf$go_names_axis1[5], pos = 3)
+text(mf$order_axis1[6,1],mf$order_axis1[6,2], label = mf$go_names_axis1[6], pos = 3)
+text(mf$order_axis1[7,1],mf$order_axis1[7,2], label = mf$go_names_axis1[7], pos = 3)
 dev.off()
