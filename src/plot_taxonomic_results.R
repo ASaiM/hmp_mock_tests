b66743b44d8d470f6fc4e3a84656263242b75132
bebatut <berenice.batut@gmail.com>
2016-03-23 15:59:04 +0100

    Add pca in plot_taxonomic_results.R

--- src/plot_taxonomic_results.R
+++ src/plot_taxonomic_results.R
@@ -1,3 +1,5 @@
+library(ade4)
+
 args<-commandArgs(TRUE)
 
 output_dir = args[1]
@@ -32,3 +34,16 @@ barplot(t(rev(species_abundances[order,c(1,3)])), width = c(1,0.5), col= col,
     beside=TRUE, horiz=TRUE, log = "x", cex.axis = 0.8, xlim = c(0.01, 50))
 legend("topright", legend = rev(c('ASaiM','Expected')), fill = rev(col), cex = 0.75)
 dev.off()
+
+family_abundances = read.table(paste(output_dir, "/family_abundance.txt", sep = ''), h = T, sep = '\t', row.names = 1)
+observed_family_abundances = family_abundances[family_abundances[,2] > 0 & family_abundances[,3] >0, ]
+family_abundances_pca = dudi.pca(observed_family_abundances, scannf = FALSE, nf = 2)
+
+pdf(paste(output_dir, "/family_abundance_pca.pdf", sep = ''))
+scatter(family_abundances_pca, clab.row = 0, posieig = "none", sub = paste('First axis (' ,
+    round(100*family_abundances_pca$eig[1]/sum(family_abundances_pca$eig)), '%) - Second axis (', 
+    round(100*family_abundances_pca$eig[2]/sum(family_abundances_pca$eig)),'%)', sep = ''))
+dev.off()
+
+print("Correlation between first axis and total abundance")
+print(cor.test(family_abundances_pca$li[,1],apply(observed_family_abundances,1,sum)))
