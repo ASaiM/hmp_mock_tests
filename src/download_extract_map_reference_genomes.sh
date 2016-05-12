4b1ccc4df7dee39a566379d1b25532f8b6d1975c
bebatut <berenice.batut@gmail.com>
2016-05-12 16:42:05 +0200

    Add script to execution mapping workflow

--- src/download_extract_map_reference_genomes.sh
+++ src/download_extract_map_reference_genomes.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 . src/misc.sh
 
+
+function launch_mapping_workflow {
+    sample_name=$1
+    result_dir="results/"$sample_name
+    output_dir=$result_dir"/mapping"
+    if [[ ! -d $output_dir ]]; then
+        mkdir $output_dir
+    fi
+
+    python src/launch_mapping_workflow.py \
+        --api_key $3 \
+        --gi_url $2 \
+        --sample_name $sample_name \
+        --output_dir $output_dir
+}
+
 echo "Download reference genomes and extract some data"
 echo "================================================"
 cd data
@@ -10,8 +26,20 @@ if [[ ! -d "reference_genomes" ]]; then
 fi
 
 cd "reference_genomes"
-python ../../src/donwload_extract_reference_genomes.py \
-    --exp_taxo_file ../expected_species_w_taxonomy.txt \
-    --protein_nb_file ../expected_species_w_protein_nb.txt
+#python "../../src/donwload_extract_reference_genomes.py" \
+#    --exp_taxo_file "../expected_species_w_taxonomy.txt" \
+#    --protein_nb_file "../expected_species_w_protein_nb.txt"
+
+if [[ -f "reference_genomes.fna.gz" ]]; then
+    rm "reference_genomes.fna.gz"
+fi
+cat *.fna.gz > "reference_genomes.fna.gz"
 cd ../../
+echo ""
+
+echo "Map raw sequences on references genomes and extract abundance information"
+echo "========================================================================="
+launch_mapping_workflow "SRR072232" $asaim_galaxy_instance_url $api_key_on_asaim_galaxy_instance
+echo ""
+launch_mapping_workflow "SRR072233" $asaim_galaxy_instance_url $api_key_on_asaim_galaxy_instance
 echo ""
---
+++ src/launch_mapping_workflow.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import argparse
+import re
+import time
+from bioblend import galaxy
+import galaxy_api_commands
+
+def launch_mapping_workflow(args):
+    workflow_file_path = "data/workflows/mapping_workflow.ga"
+    gi = galaxy_api_commands.connect_to_galaxy_instance(args.gi_url, args.api_key)
+
+    input_filepaths = {}
+    input_filepaths['raw sequences'] = "data/" + args.sample_name + ".fastq"
+    input_filepaths['reference genomes'] = "data/reference_genomes/reference_genomes.fna.gz"
+
+    file_types = {}
+    file_types['raw sequences'] = "fastqsanger"
+    file_types['reference genomes'] = "fasta"
+
+    galaxy_api_commands.run_workflow('Mapping for ' + args.sample_name, 
+        workflow_file_path, input_filepaths, gi, args.output_dir, export = True, 
+        file_types = file_types, check_history_state = True)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gi_url', required=True)
+    parser.add_argument('--api_key', required=True)
+    parser.add_argument('--sample_name', required=True)
+    parser.add_argument('--output_dir', required=True)
+    args = parser.parse_args()
+
+    launch_mapping_workflow(args)
