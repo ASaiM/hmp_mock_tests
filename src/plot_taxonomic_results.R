8d1a40498fc97edb3603eaed85d8f50c4f201b0c
bebatut <berenice.batut@gmail.com>
2016-05-20 11:41:27 +0200

    Modify plotting attributes for taxonomic result comparison

--- src/plot_taxonomic_results.R
+++ src/plot_taxonomic_results.R
@@ -15,11 +15,11 @@ pdf(paste(output_dir, "/family_abundances.pdf", sep = ""),width = 5, height = 5)
 par(las=2)
 par(mar=c(5,10,1,1))
 col = c("blue3","green3", "red")
-order = c(12,15,1,5,6,7,10,14,4,8,9,11,16,18,2,3,13,17,19)
+order = order(family_abundances[,1], decreasing = TRUE)#c(12,15,1,5,6,7,10,14,4,8,9,11,16,18,2,3,13,17,19)
 barplot(t(rev(family_abundances[order,])), 
     width = c(1,1,0.5), col= col, xlab = "Relative abundances in percentage", 
     beside=TRUE, horiz=TRUE, log = "x", cex.axis = 0.8, xlim = c(0.01, 50))
-legend("topright", legend = rev(c('ASaiM','EBI','Expected')), fill = rev(col), cex = 0.7)
+legend("topright", legend = rev(c('ASaiM','EBI','Mapping-based')), fill = rev(col), cex = 0.7)
 dev.off()
 
 species_abundances = read.table(paste(output_dir, "/species_abundance.txt", sep = ''), h = T, sep = '\t', row.names = 1)
@@ -28,16 +28,18 @@ pdf(paste(output_dir, "/species_abundances.pdf", sep = ""))
 par(las=2)
 par(mar=c(5,16,1,1))
 col = c("blue3","red")
-order = c(1,5,6,12,17,7,10,14,16,18,4,8,9,11,19,21,2,3,13,15,20,22)
+order = order(species_abundances[,1], decreasing = TRUE)#c(1,5,6,12,17,7,10,14,16,18,4,8,9,11,19,21,2,3,13,15,20,22)
 barplot(t(rev(species_abundances[order,c(1,3)])), width = c(1,0.5), col= col, 
     xlab = "Relative abundances in percentage",
     beside=TRUE, horiz=TRUE, log = "x", cex.axis = 0.8, xlim = c(0.01, 50))
-legend("topright", legend = rev(c('ASaiM','Expected')), fill = rev(col), cex = 0.75)
+legend("topright", legend = rev(c('ASaiM','Mapping-based')), fill = rev(col), cex = 0.75)
 dev.off()
 
 family_abundances = read.table(paste(output_dir, "/family_abundance.txt", sep = ''), h = T, sep = '\t', row.names = 1)
+colnames(family_abundances) = c("Mapping-based", "EBI", "ASaiM")
 observed_family_abundances = family_abundances[family_abundances[,2] > 0 & family_abundances[,3] >0, ]
 family_abundances_pca = dudi.pca(observed_family_abundances, scannf = FALSE, nf = 2)
+
 
 pdf(paste(output_dir, "/family_abundance_pca.pdf", sep = ''))
 scatter(family_abundances_pca, clab.row = 0, posieig = "none", sub = paste('First axis (' ,
