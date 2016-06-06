2a71a47ddde703e654251546cd55c9987be9e9ec
bebatut <berenice.batut@gmail.com>
2016-06-06 17:25:45 +0200

    Add pcoa plots and update some other plots

--- doc/report/build.sh
+++ doc/report/build.sh
@@ -26,47 +26,14 @@ if $image_update ; then
         "doc/images/SRR072232/concatenated_family_abundances.pdf"
     cp "results/SRR072233/concatenated_results/family_abundances.pdf" \
         "doc/images/SRR072233/concatenated_family_abundances.pdf"
-    cp "results/SRR072232/concatenated_results/family_abundance_pca.pdf" \
-        "doc/images/SRR072232/concatenated_family_abundance_pca.pdf"
-    cp "results/SRR072233/concatenated_results/family_abundance_pca.pdf" \
-        "doc/images/SRR072233/concatenated_family_abundance_pca.pdf"
-
-    cp $SRR072232_result_dir"/asaim_results/60_plot_generic_x-y_plot_on_data_53_pdf_x-y_plot.pdf" \
-        "doc/images/SRR072232/gene_family_nb.pdf"
-    cp $SRR072232_result_dir"/asaim_results/58_plot_generic_x-y_plot_on_data_53_pdf_x-y_plot.pdf" \
-        "doc/images/SRR072232/mean_gene_family_abundance.pdf"
-    cp $SRR072232_result_dir"/asaim_results/62_plot_generic_x-y_plot_on_data_54_pdf_x-y_plot.pdf" \
-        "doc/images/SRR072232/pathway_nb.pdf"
-    cp $SRR072232_result_dir"/asaim_results/61_plot_generic_x-y_plot_on_data_54_pdf_x-y_plot.pdf" \
-        "doc/images/SRR072232/mean_pathway_abundance.pdf"
-    cp $SRR072232_result_dir"/asaim_results/plot_prot_nb_gene_family_number.pdf" \
-        "doc/images/SRR072232/gene_family_nb_protein_nb.pdf"
-    cp $SRR072232_result_dir"/asaim_results/plot_diff_prot_nb_gene_family_number.pdf" \
-        "doc/images/SRR072232/diff_prot_nb_gene_family_number.pdf"
-
-    cp $SRR072233_result_dir"/asaim_results/plot_diff_prot_nb_gene_family_number.pdf" \
-        "doc/images/SRR072233/diff_prot_nb_gene_family_number.pdf"
-
-    cp $SRR072232_result_dir"/asaim_results/64_plot_barplot_on_data_55_pdf_barplot.pdf" \
-        "doc/images/SRR072232/molecular_functions.pdf"
-    cp $SRR072232_result_dir"/asaim_results/65_plot_barplot_on_data_56_pdf_barplot.pdf" \
-        "doc/images/SRR072232/biological_processes.pdf"
-    cp $SRR072232_result_dir"/asaim_results/66_plot_barplot_on_data_57_pdf_barplot.pdf" \
-        "doc/images/SRR072232/cellular_components.pdf"
-
-    cp $concatenated_results_dir"/gene_families/15_similar_charact_x_y_plot.pdf" \
-        "doc/images/concatenated_samples/gene_families_SRR072232_SRR072233.pdf" 
-    cp $concatenated_results_dir"/pathways/15_similar_charact_x_y_plot.pdf" \
-        "doc/images/concatenated_samples/pathways_SRR072232_SRR072233.pdf" 
-
-    cp $concatenated_results_dir"/gene_families/36_plot_diff_orga_abund_diff_charact_nb.pdf" \
-        "doc/images/concatenated_samples/gene_family_nb.pdf" 
-    cp $concatenated_results_dir"/gene_families/42_plot_diff_orga_abund_diff_charat_mean_abund.pdf" \
-        "doc/images/concatenated_samples/mean_gene_family_abundance.pdf" 
-    cp $concatenated_results_dir"/pathways/37_plot_diff_orga_abund_diff_charact_nb.pdf" \
-        "doc/images/concatenated_samples/pathway_nb.pdf" 
-    cp $concatenated_results_dir"/pathways/42_plot_diff_orga_abund_diff_charat_mean_abund.pdf" \
-        "doc/images/concatenated_samples/mean_pathway_abundance.pdf" 
+    cp "results/SRR072232/concatenated_results/family_abundance_pcoa.pdf" \
+        "doc/images/SRR072232/concatenated_family_abundance_pcoa.pdf"
+    cp "results/SRR072233/concatenated_results/family_abundance_pcoa.pdf" \
+        "doc/images/SRR072233/concatenated_family_abundance_pcoa.pdf"
+        cp "results/SRR072232/concatenated_results/species_abundance_pcoa.pdf" \
+            "doc/images/SRR072232/concatenated_species_abundance_pcoa.pdf"
+        cp "results/SRR072233/concatenated_results/species_abundance_pcoa.pdf" \
+            "doc/images/SRR072233/concatenated_species_abundance_pcoa.pdf"
 
     # Functional results
     ## Only ASaiM
